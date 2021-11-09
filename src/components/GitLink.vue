b02aa3d439635b8a95534a3e42bd9ecf8ea1ef01
Aaron Kebede <93286998+AaronCTP@users.noreply.github.com>
2021-11-09 01:23:25 -0500

    Update GitLink.vue

--- src/components/GitLink.vue
+++ src/components/GitLink.vue
@@ -1,5 +1,5 @@
 <template>
-  <a href="https://github.com/samuelhorn/jamdocs" :class="size" title="Git-repository" aria-label="Jamdocs on Github">
+  <a href="https://github.com/aaronctp/docs" :class="size" title="Git-repository" aria-label="CTPdocs on Github">
     <github-icon class="icon" />
     {{text}}
   </a>
@@ -19,7 +19,7 @@ export default {
     },
     text: {
       type: String,
-      default: 'Fork me on Github'
+      default: 'Github'
     }
   }
 }
